f3145a2ceba26c393ead7515416252c8ee940098
JasonDollar <elrey33@gmail.com>
2020-02-23 13:52:54 +0100

    added database connection with mongoose

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
+require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
+
 const placeRouter = require('./routes/places-routes')
 
 const app = express()
 
-app.use('/', placeRouter)
+app.use('/api/places', placeRouter)
 
-app.listen(5000)
+
+mongoose.connect(process.env.MONGO_URI, { 
+  useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true
+})
+  .then(() => console.log('db connected'))
+  .catch(e => console.log(e))
+
+  
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+  console.log('server started')
+})
