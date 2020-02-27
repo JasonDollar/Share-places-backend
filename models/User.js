02dc057d2f1b3715ab4ef51b513bee620ca0f24e
JasonDollar <elrey33@gmail.com>
2020-02-27 14:04:34 +0100

    added mongoose-unique-validator

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const { Schema } = mongoose
 
@@ -19,6 +20,8 @@ const userSchema = new Schema({
     { type: mongoose.Types.ObjectId, required: true, ref: 'Place' },
   ],
 })
+
+userSchema.plugin(uniqueValidator)
 
 
 module.exports = mongoose.model('User', userSchema)
